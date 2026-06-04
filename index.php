bb4a3a24054d06efcf1e7d899bdf7f70c80ba20f
GWServer <33991099+GWServer@users.noreply.github.com>
2026-06-04 22:08:58 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// Простий PHP-файл з посиланням
+$site = "https://mc-servera.net";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Посилання для мониторингу</title>
+</head>
+<body>
+    <h1>Моє посилання</h1>
+    <a href="<?php echo $site; ?>">mc-servera.net</a>
+    <p>Сторінка згенерована PHP</p>
+</body>
+</html>
